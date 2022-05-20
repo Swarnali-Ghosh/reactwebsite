63b863584b34cd0cf4309bf2e8564fc806efd9ee
Swarnali Ghosh <95978425+Swarnali-Ghosh@users.noreply.github.com>
2022-05-21 01:54:29 +0530

    Update DoctorSection.js

--- src/DoctorSection/DoctorSection.js
+++ src/DoctorSection/DoctorSection.js
@@ -1,12 +1,19 @@
 import React, {useState} from 'react';
 import Doctordata from './Doctordata'
 import { Link } from 'react-router-dom';
-
 import './DoctorSection.css'
 const DoctorSection = () => {
   const [data] = useState(Doctordata);
   const [query,setQuery] = useState("");
   document.title = `Our Doctors`
+
+  const filteredData = data.filter(({ d_name }) => {
+    if (d_name.toLowerCase().includes(query.toLowerCase())) {
+      return true;
+    } else {
+      return false;
+    }
+  });
   return (
     <>
    
@@ -15,38 +22,27 @@ const DoctorSection = () => {
           <input className='searchbox' placeholder="Search by doctor name..." type="text" onChange={e=>setQuery(e.target.value)} />
       </div>
 
-
+      {query && filteredData.length === 0 && (
+        <div className='sorry'>Sorry, this name does not exist</div>
+      )}
         <div className='wrapper' id="doctor">
-            {data.filter((val)=>{
-              if(query === " "){
-                return val
-              }
-              else if(val.d_name.toLowerCase().includes(query.toLowerCase())){
-                return val
-              }
-              else{
-                return false
-              }
-            
-            })
+            {filteredData
             .map((values) => {
-                const { id, src, d_name, d_info, primary } = values;
                 return (
-                        <div className="doctor" key={id}>
-                            <img className="doctor-img" src={src} alt={d_name}/>
-                            <span className="d_name"><b>{d_name}</b></span>
-                            <span className="d_info">{d_info}</span>
+                        <div className="doctor" key={values.id}>
+                            <img className="doctor-img" src={values.src} alt={values.d_name}/>
+                            <span className="d_name"><b>{values.d_name}</b></span>
+                            <span className="d_info">{values.d_info}</span>
                             <div>
                                    <div><button className="primary" 
                                     onClick={()=>{alert("Call us  now on 000 0000 0000 or 111 1111 1111 for booking an appoimentment")}}>
-                                    {primary}</button> </div> 
+                                    {values.primary}</button> </div> 
                             </div>
                         </div>
                 );
 
             })}
         </div>
-    
     </>
   )
 }
